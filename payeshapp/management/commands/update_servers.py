b8b6b034d9f25ef62539d6cfc306b47e64460636
Mahsa_Gol <mahsa.golchian@ut.ac.ir>
2018-08-01 13:47:05 +0430

    "change in some items"

--- payeshapp/management/commands/update_servers.py
+++ payeshapp/management/commands/update_servers.py
@@ -29,7 +29,7 @@ def save_host(length, query):
 
 
 def login():
-    zapi = ZabbixAPI("http://172.16.16.157:4720")
+    zapi = ZabbixAPI("http://192.168.112.157:4720")
     zapi.login("riri", "rayta")
     return zapi
 
