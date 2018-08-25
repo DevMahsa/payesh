3e60b3cbbdf8458c2439d1580fb9f4d3c92ec402
Mahsa_Gol <mahsa.golchian@ut.ac.ir>
2018-08-25 16:01:16 +0430

    "change in some items"

--- payeshapp/admin.py
+++ payeshapp/admin.py
@@ -9,10 +9,7 @@ from .models import Server, LastMemory, LastCpu
 @admin.register(Server)
 class ServerModelAdmin(admin.ModelAdmin):
     actions = ["export_as_csv"]
-    list_display = (
-        'name', 'date', 'firewall', 'mcafee', 'telnet', 'anydesk','time_win_sync','smb1_config','file_sharing_port','new_system_event','new_app_event','microsoft_update','local_user','maxusedmemory', 'maxusedcpu', 'freediskc', 'freediskd',
-        'freediske', 'freediskf', 'freediskg',
-        'freediskh', 'freediski','ssl_cert_exp','win_active','sql_login_user', 'sql_xp_cmdshell','sql_version', 'sql_file_size','backup_name','windows_version','open_port', 'eventlog_max_size')
+    #list_display = ('__all__')
     search_fields = ('name','ip')
 
     def export_as_csv(self, request, queryset):
@@ -25,7 +22,7 @@ class ServerModelAdmin(admin.ModelAdmin):
 
     def response_write_to_csv(self, field_names, queryset, meta):
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment;  filename=%s.csv' % str(queryset[0]).replace('.', '_')
+        response['Content-Disposition'] = 'attachment;  filename=%s.csv' % str(queryset[0]) .replace('.', '_')
         writer = csv.writer(response)
         # writer.writerow(field_names)
         self.wirte_rows(field_names, queryset, writer)
@@ -34,6 +31,7 @@ class ServerModelAdmin(admin.ModelAdmin):
         return response
 
     def wirte_rows(self, field_names, queryset, writer):
+
         for field in field_names:
             row = [field]
             for obj in queryset:
