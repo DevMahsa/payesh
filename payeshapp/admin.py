b1d5c4d54ef5f05572814611f22916fad41bc091
Mahsa_Gol <mahsa.golchian@ut.ac.ir>
2018-08-25 14:52:11 +0430

    "change in some items"

--- payeshapp/admin.py
+++ payeshapp/admin.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+
 from django.contrib import admin
 from django.http import HttpResponse
 from .models import Server, LastMemory, LastCpu
@@ -15,14 +16,16 @@ class ServerModelAdmin(admin.ModelAdmin):
     search_fields = ('name','ip')
 
     def export_as_csv(self, request, queryset):
+        list  = self.list_display
         meta = self.model._meta
+        field_names = [field for field in list]
         field_names = [field.name for field in meta.fields]
 
-        return self.response_write_to_csv(field_names, queryset)
+        return self.response_write_to_csv(field_names, queryset, meta)
 
-    def response_write_to_csv(self, field_names, queryset):
+    def response_write_to_csv(self, field_names, queryset, meta):
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(datetime.now())
+        response['Content-Disposition'] = 'attachment;  filename=%s.csv' % str(queryset[0]).replace('.', '_')
         writer = csv.writer(response)
         # writer.writerow(field_names)
         self.wirte_rows(field_names, queryset, writer)
