3ac8e55a5d37fa3d62b7233e0b6944e494f3714f
Mahsa_Gol <mahsa.golchian@ut.ac.ir>
2018-09-01 10:14:01 +0430

    "change in some items"

--- payeshapp/management/commands/clean.py
+++ payeshapp/management/commands/clean.py
@@ -1,72 +1,7 @@
 from payeshapp.models import *
 
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'rtisdb')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'rtisdb')).all().delete()
-WindowsServer.objects.get(name = 'rtisdb').delete()
+LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'liferay-new')).all().delete()
+LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'liferay-new')).all().delete()
+WindowsServer.objects.get(name = 'liferay-new').delete()
 
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'always on db')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'always on db')).all().delete()
-WindowsServer.objects.get(name = 'always on db').delete()
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'rtis-platform')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'rtis-platform')).all().delete()
-WindowsServer.objects.get(name = 'rtis-platform').delete()
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'rtisapp')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'rtisapp')).all().delete()
-WindowsServer.objects.get(name = 'rtisapp').delete()
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'diningapp182')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'diningapp182')).all().delete()
-WindowsServer.objects.get(name = 'diningapp182').delete()
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'dining180')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'dining180')).all().delete()
-WindowsServer.objects.get(name = 'dining180').delete()
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'diningdb181')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'diningdb181')).all().delete()
-WindowsServer.objects.get(name = 'diningdb181').delete()
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'elk')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'elk')).all().delete()
-WindowsServer.objects.get(name = 'elk').delete()
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'sdr')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'sdr')).all().delete()
-WindowsServer.objects.get(name = 'sdr').delete()
-
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'dcomd')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'dcomd')).all().delete()
-WindowsServer.objects.get(name = 'dcomd').delete()
-
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'LTC')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'LTC')).all().delete()
-WindowsServer.objects.get(name = 'LTC').delete()
-
-
-
-
-
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'plesk_linux_244')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'plesk_linux_244')).all().delete()
-WindowsServer.objects.get(name = 'plesk_linux_244').delete()
 
