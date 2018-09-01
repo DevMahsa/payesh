8f56258c96be8976da069c03d1977f1b7499e495
Mahsa_Gol <mahsa.golchian@ut.ac.ir>
2018-09-01 10:17:05 +0430

    "change in some items"

--- payeshapp/management/commands/clean.py
+++ payeshapp/management/commands/clean.py
@@ -1,7 +1,13 @@
 from payeshapp.models import *
+from django.core.management.base import BaseCommand
 
-LastCpu.objects.filter(server = WindowsServer.objects.get(name = 'liferay-new')).all().delete()
-LastMemory.objects.filter(server = WindowsServer.objects.get(name = 'liferay-new')).all().delete()
-WindowsServer.objects.get(name = 'liferay-new').delete()
+class Command(BaseCommand):
+    def handle(self, **options):
+        clean()
+
+def clean():
+    LastCpu.objects.filter(server=WindowsServer.objects.get(name='liferay-new')).all().delete()
+    LastMemory.objects.filter(server=WindowsServer.objects.get(name='liferay-new')).all().delete()
+    WindowsServer.objects.get(name='liferay-new').delete()
 
 
