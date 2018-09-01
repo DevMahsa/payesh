3f32a2d4576f596fe317bcb8d661dd5afca2422e
Mahsa_Gol <mahsa.golchian@ut.ac.ir>
2018-09-01 10:57:48 +0430

    "change in some items"

---
+++ payeshapp/management/commands/update_linux_servers.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from pyzabbix import ZabbixAPI
+from payeshapp.models import LinuxServer
+
+
+def update():
+    zapi = login()
+    length, query = select_host(zapi)
+    save_host(length, query)
+
+
+def select_host(zapi):
+    query = zapi.host.get(output="extend", selectInterfaces="extend", selectParentTemplates="Array")
+    length = query.__len__()
+    return length, query
+
+
+def save_host(length, query):
+    for count in range(length):
+        if LinuxServer.objects.filter(name=query[count]['name']).exists():
+            count += 1
+            continue
+        get = LinuxServer(name=query[count]['name'])
+        get.ip = query[count]['interfaces'][0]['ip']
+        get.name = query[count]['name']
+        get.hostid = query[count]['hostid']
+        get.save()
+
+
+def login():
+    zapi = ZabbixAPI("http://192.168.112.157:4720")
+    zapi.login("ririlinux", "ririlinux")
+    return zapi
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        update()
